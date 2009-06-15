01e752f583f2ca0d5804a366378ac488cf2b5c38
Eishay Smith <eishay+github@gmail.com>
2009-06-14 19:42:36 -0700

    added more scala support

--- src/com/newspipes/server/core/TwitterSearch.scala
+++ src/com/newspipes/server/core/TwitterSearch.scala
@@ -9,19 +9,32 @@ class TwitterSearch {
     val urlFetchService = URLFetchServiceFactory.getURLFetchService
     val response = urlFetchService.fetch(new URL("http://search.twitter.com/search.atom?q=" + query + "&rpp=10"))
     val stringValue = new String(response.getContent)
+    println("got = " + stringValue)
     val xml = XML.loadString(stringValue)
-    val urls = extractUrls(xml)
-    urls.mkString
+    val urls = extractUrls(xml \ "entry")
+    val results = urls.mkString
+    println("results = " + results)
+    results
   }
   
-  def extractUrls(xml:  Elem) : Seq[String] = {
-    xml.child map(extractUrl) filter {case None => false; case _ => true} map {case Some(url) => url; case None => throw new RuntimeException("impossible reach")}
+  def extractUrls(xml:  NodeSeq) : Seq[String] = {
+    xml map (n => extractUrl(n)) flatMap (a => a) filter {case None => false; case _ => true} map {case Some(url) => url; case None => throw new RuntimeException("impossible reach")}
   }
   
-  def extractUrl(element: Node) : Option[String] = element match {
-    case <entry><content>{ Text(content) }</content></entry> => extractUrl(content) 
-    case _ => None
+  def extractUrl(element: Node) : Seq[Option[String]] = {
+    element \ "content" map (a => extractUrlFromContent(a.toString)) 
   }
   
-  def extractUrl(content: String) : Option[String] = Some(content)
+  case class ExternalLink {
+    import java.util.regex.Pattern._
+    
+    val urlPattern = compile(""".*a href="(.*)".*""") 
+    def unapply(str: String): Option[String] = {
+      
+    }
+  }
+  
+  def extractUrlFromContent(content: String) : Option[String] = content match {
+    case Some(content)
+  }
 }
